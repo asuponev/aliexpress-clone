4ae5432985c1321f0e6696387d072546f9de233f
Andrew <andrew.suponev@gmail.com>
2023-06-07 12:41:07 +0400

    feat: implement nuxt server API with prisma and supabase

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -115,11 +115,18 @@ const route = useRoute()
 let product = ref(null)
 let currentImage = ref(null)
 
-onMounted(() => {
-  watchEffect(() => {
-    ;(currentImage.value = 'https://picsum.photos/id/77/800/800'),
-      (images.value[0] = 'https://picsum.photos/id/77/800/800')
-  })
+onBeforeMount(async () => {
+  product.value = await useFetch(
+    `/api/prisma/get-product-by-id/${route.params.id}`
+  )
+})
+
+watchEffect(() => {
+  if (product.value && product.value.data) {
+    currentImage.value = product.value.data.url
+    images.value[0] = product.value.data.url
+    userStore.isLoading = false
+  }
 })
 
 const isInCart = computed(() => {
@@ -149,6 +156,6 @@ const images = ref([
 ])
 
 const addToCart = () => {
-  alert('ADDED')
+  userStore.cart.push(product.value.data)
 }
 </script>
